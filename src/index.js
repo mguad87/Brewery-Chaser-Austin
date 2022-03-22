250ad162235f1bfebb9c025cd3788346c766b6ce
mguad87 <68387546+mguad87@users.noreply.github.com>
2022-03-22 12:18:34 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//JS code goes here
+
+//fetch GET request that grabs API data
+const breweryURL = "https://api.openbrewerydb.org/breweries?by_city=austin"
+
+
+
+//function to create cards from fetch data
+
+
+
+//clickevent for button
+
+
+//submit event for comment
